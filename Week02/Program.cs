c1df9a79035f1bdc90b799949762f84f88cdc7a4
User <User@DEV-022834.OnBase.net>
2019-02-12 19:23:50 -0500

    this is a me page

--- Week02/Program.cs
+++ Week02/Program.cs
@@ -7,7 +7,31 @@ namespace Week02
         static void Main(string[] args)
         {
             // Create variables and do things with them!
-            
+            string myName = "Tevyah";
+
+            int myAge = 15;
+
+            string favColor = "blue";
+
+            string myHobby = "The arts";
+
+            bool isOldEnoughToDrive = true;
+
+            double myheight = 5.5;
+
+            char FirstInitial = 't';
+
+            Console.WriteLine("My name is " + myName);
+
+            Console.WriteLine("My age is " + myAge);
+
+            Console.WriteLine("My favorite hobby is " + myHobby);
+
+              Console.WriteLine("My height is " + myheight + " feet");
+
+              Console.WriteLine("My first intitial is " + FirstInitial);
+
+              Console.WriteLine("I am old enough to drive " + isOldEnoughToDrive);
         }
     }
 }
