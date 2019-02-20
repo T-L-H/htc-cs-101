e04e9627f02b5b59742454fd048770190e04164c
User <User@DEV-022776.OnBase.net>
2019-02-19 19:28:08 -0500

    Created program that asks for temperture input. a 1 to 10 guessing game

---
+++ Week02/week03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace week03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//             Console.WriteLine("Hello World!");
+
+//              Console.WriteLine("What is the temperature outside?");
+//              int OutsideTemperature = Convert.ToInt32(Console.ReadLine());
+
+//              if (OutsideTemperature <= 25){
+// Console.WriteLine("Put on a coat!");
+
+//              }
+
+//             else if(OutsideTemperature >= 26) 
+//              {
+//                 Console.WriteLine("Wear sunglasses!"); 
+//              }
+
+//              else {Console.WriteLine("Try not to die!");
+//              }
+
+                Console.WriteLine("Guess a number between 1 and 10");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number > 4){
+                Console.WriteLine("Too High!");}
+
+               else if (number < 4){
+                Console.WriteLine("Too Low!");}
+
+                
+                else{
+                Console.WriteLine("Just Right!");}
+
+
+        }
+    }
+}
